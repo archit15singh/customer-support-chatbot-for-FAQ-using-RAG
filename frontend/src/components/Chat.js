99b4bbe490c7dd08034d6ec102c7204aa3ceb2f9
Archit Singh <contact.architsingh@gmail.com>
2023-10-19 13:19:56 +0530

    update: height of chat box should be responsive based on viewport size

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -8,25 +8,25 @@ import {
   MessageInput
 } from '@chatscope/chat-ui-kit-react';
 
+
 const Chat = () => {
   return (
-    <div style={{ position: "relative", height: "500px" }}>
-      <MainContainer>
+    <MainContainer style={{ position: "relative", height: "70vh" }}>
         <ChatContainer>
-          <MessageList>
+            <MessageList>
             <Message
-              model={{
-                message: "Hello my friend",
-                sentTime: "just now",
-                sender: "Joe"
-              }}
+                model={{
+                    message: "Hello my friend",
+                    sentTime: "just now",
+                    sender: "Joe"
+                }}
             />
-          </MessageList>
-          <MessageInput placeholder="Type message here" />
+            </MessageList>
+            <MessageInput placeholder="Type message here" />
         </ChatContainer>
-      </MainContainer>
-    </div>
+    </MainContainer>
   );
 };
+
 
 export default Chat;
