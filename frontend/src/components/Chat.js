60b6baff283195b44b5f20e483418a69214262cf
Archit Singh <contact.architsingh@gmail.com>
2023-10-19 13:04:49 +0530

    new: add initial chat screen

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput
+} from '@chatscope/chat-ui-kit-react';
+
+const Chat = () => {
+  return (
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe"
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
+};
+
+export default Chat;
