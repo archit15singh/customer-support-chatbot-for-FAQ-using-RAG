8f9be49a81b7add14f259264ecbc076e4eeabbd6
Archit Singh <contact.architsingh@gmail.com>
2023-10-20 18:21:14 +0530

    new: add conversational memory

--- backend/app.py
+++ backend/app.py
@@ -4,24 +4,38 @@ import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
+
 
 LLM_API_URL = "http://localhost:11434/api/generate"
 
 
-def generate_response(question):
+def format_conversation_history(conversation_history):
+    processed_conversation_history = ""
+
+    for message in conversation_history:
+        processed_conversation_history += f"{message['sender']}: {message['message']}\n"
+    return processed_conversation_history
+
+
+def generate_response(question, conversation_history):
     instruction = """
-    You are a Twitter customer support AI chatbot.
-    Only answer questions related to Twitter customer support.
-    If the question is not related to Twitter customer support, do not answer the question.
+    You are a Twitter customer support AI chatbot with memory of conversation history.
+    Answer questions related to Twitter customer support.
+    Answer questions based on the conversation history.
+    If the question is not related to Twitter customer support or the conversation history, do not answer the question.
+
+    Conversation History:
+    $conversation_history
 
     Answer the following question if its related to twitter customer support otherwise say I know about twitter only:
     $question
     """
     
     template = Template(instruction)
-    prompt = template.substitute(question=question)
+    prompt = template.substitute(conversation_history=conversation_history, question=question)
 
     data = {
         "model": "mistral-openorca",
@@ -38,17 +52,21 @@ def generate_response(question):
     except requests.RequestException as e:
         return f"Error calling external API: {str(e)}"
 
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('user_message')
+    conversation_history = request.json.get('conversation_history')
 
-    if user_message is None:
-        return jsonify({'error': 'Missing user_message parameter'}), 400
-
-    response = generate_response(user_message)
+    if user_message is None or conversation_history is None:
+        return jsonify({'error': 'Missing parameter conversation_history or user_message'}), 400
+    
+    conversation_history = format_conversation_history(conversation_history)
+    response = generate_response(user_message, conversation_history)
     response = response.strip()
 
     return jsonify({'response': response})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
